bc63122b1bc8bb2c603a1a3d7324e1684240d446
poxu <poxvuibr@gmail.com>
2015-10-14 18:35:20 +0300

    Tests for async javascript

---
+++ src/runners.js
@@ -0,0 +1,48 @@
+var runAsync = (function() {
+    return function(callbacks, times) {
+        var cnt = times;
+
+        if (callbacks.worker) {
+            var worker = function() {
+                callbacks.worker();
+                cnt -= 1;
+                if (cnt === 0) {
+                    if(callbacks.onEnd) {
+                        callbacks.onEnd();
+                    }
+                }
+            };
+            
+            for(var i = 0;i<times; ++i) {
+                setTimeout(worker);
+            }
+        }
+
+    };
+}());
+
+var runSeq = (function() {
+    return function(callbacks, times) {
+        var cnt = times;
+
+        if (callbacks.worker) {
+            var worker = callbacks.worker;
+            
+            for(var i = 0;i<times; ++i) {
+                worker();
+            }
+        }
+
+        if(callbacks.onEnd) {
+            callbacks.onEnd();
+        }
+
+    };
+}());
+
+if (typeof module !== 'undefined') {
+    module.exports = {
+        runAsync: runAsync,
+        runSeq: runSeq
+    };
+}
---
+++ test/runnersTest.js
@@ -0,0 +1,77 @@
+var expect = require('expect.js');
+
+var exports = require('../src/runners.js');
+
+var runAsync = exports.runAsync;
+
+var functionName = function (fun) {
+  var ret = fun.toString();
+  ret = ret.substr('function '.length);
+  ret = ret.substr(0, ret.indexOf('('));
+  return ret;
+};
+
+var getTime = function() {
+    return new Date().getTime();
+};
+
+describe('Runners', function() {
+    var functions = Object.keys(exports);
+
+    functions.forEach(function(funcName) {
+        var func = exports[funcName];
+
+    describe('#' + funcName, function() {
+        it('should run final callback', function(done) {
+            func({
+                worker: function() {},
+                onEnd: function() {
+                    done();
+                }
+            }, 10);
+
+        });
+
+        it('should run worket 10 times and run callback after all workers are done', function(done) {
+            var times = 10;
+            func({
+                worker: function() {
+                    times -= 1;
+                },
+                onEnd: function() {
+                    expect(times).to.be.equal(0);
+                    done();
+                }
+            }, times);
+        });
+
+
+        it('should not run callbacks simultaniously', function(done) {
+            this.timeout(10000);
+            this.slow(20000);
+            var startTime = getTime();
+            var lastCallbackTime = startTime;
+            var workersTime = 0;
+
+            func({
+                worker: function() {
+                    var currTime = getTime();
+
+                    for(var cnt = getTime(); getTime() - cnt < 1000;);
+
+                    workersTime += (currTime - lastCallbackTime);
+                    lastCallbackTime = currTime;
+                },
+                onEnd: function() {
+                    var endTime = getTime();
+                    var executorTime = endTime - startTime;
+                    expect(executorTime).to.be.within(workersTime, Infinity);
+                    done();
+                }
+            }, 5);
+        });
+    });
+    });
+});
+
+
